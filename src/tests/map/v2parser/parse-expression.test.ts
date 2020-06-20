4bd3b374fd61387e04fd9289c8b22528cf14b29e
aoisupersix <aoisupersix@gmail.com>
2020-06-21 08:29:27 +0900

    [add] var, string, distance expression parse test

--- src/tests/map/v2parser/parse-expression.test.ts
+++ src/tests/map/v2parser/parse-expression.test.ts
@@ -1,14 +1,45 @@
 import { execParseExpression, assertMapAstNode } from './util'
-import { NumberNode, NodeType } from '#/map/ast-nodes'
+import * as ast from '#/map/ast-nodes'
 
 describe('MapV2Parser', () => {
-  describe('#parse', () => {
+  describe('#parse_unary_expressions', () => {
+    it('var', () => {
+      const variable = execParseExpression('$aBC_0;') as ast.VarNode
+      expect(variable).toBeTruthy()
+      expect(variable.type).toBe(ast.NodeType.Var)
+      expect(variable.varName).toBe('aBC_0')
+      assertMapAstNode(variable, 1, 0, 1, 5, '$aBC_0')
+    })
+
     it('number', () => {
-      const number = execParseExpression('100;') as NumberNode
+      const number = execParseExpression('100;') as ast.NumberNode
       expect(number).toBeTruthy()
-      expect(number.type).toBe(NodeType.Number)
+      expect(number.type).toBe(ast.NodeType.Number)
       expect(number.value).toBe('100')
       assertMapAstNode(number, 1, 0, 1, 2, '100')
+    })
+
+    it('empty string', () => {
+      const string = execParseExpression(`'';`) as ast.StringNode
+      expect(string).toBeTruthy()
+      expect(string.type).toBe(ast.NodeType.String)
+      expect(string.value).toBe('')
+      assertMapAstNode(string, 1, 0, 1, 1, `''`)
+    })
+
+    it('reserved word string', () => {
+      const string = execParseExpression(`'Curve';`) as ast.StringNode
+      expect(string).toBeTruthy()
+      expect(string.type).toBe(ast.NodeType.String)
+      expect(string.value).toBe('Curve')
+      assertMapAstNode(string, 1, 0, 1, 6, `'Curve'`)
+    })
+
+    it('distance', () => {
+      const distance = execParseExpression('dIsTAnce;') as ast.DistanceNode
+      expect(distance).toBeTruthy()
+      expect(distance.type).toBe(ast.NodeType.Distance)
+      assertMapAstNode(distance, 1, 0, 1, 7, 'dIsTAnce')
     })
   })
 })

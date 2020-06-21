0629c54c74374f8a3b816b8a4fbf01559150d494
aoisupersix <aoisupersix@gmail.com>
2020-06-21 17:31:58 +0900

    [add] math function expressions test

--- src/tests/map/v2parser/parse-expression.test.ts
+++ src/tests/map/v2parser/parse-expression.test.ts
@@ -68,6 +68,104 @@ describe('MapV2Parser', () => {
       assertMapAstNode(modulo.right, 1, 4, 1, 4, '1')
     })
 
+    it('abs', () => {
+      const abs = execParseExpression('abs(0);') as ast.AbsNode
+      expect(abs).toBeTruthy()
+      expect(abs.type).toBe(ast.NodeType.Abs)
+      assertMapAstNode(abs, 1, 0, 1, 6, 'abs(1)')
+      assertMapAstNode(abs.value, 1, 5, 1, 5, '1')
+    })
+
+    it('atan2', () => {
+      const atan2 = execParseExpression('atan2(0,1);') as ast.Atan2Node
+      expect(atan2).toBeTruthy()
+      expect(atan2.type).toBe(ast.NodeType.Atan2)
+      assertMapAstNode(atan2, 1, 0, 1, 9, 'atan2(0,1)')
+      assertMapAstNode(atan2.y, 1, 6, 1, 6, '0')
+      assertMapAstNode(atan2.x, 1, 8, 1, 8, '1')
+    })
+
+    it('ceil', () => {
+      const ceil = execParseExpression('ceil(0);') as ast.CeilNode
+      expect(ceil).toBeTruthy()
+      expect(ceil.type).toBe(ast.NodeType.Ceil)
+      assertMapAstNode(ceil, 1, 0, 1, 6, 'ceil(0)')
+      assertMapAstNode(ceil.value, 1, 5, 1, 5, '0')
+    })
+
+    it('cos', () => {
+      const cos = execParseExpression('cos(0);') as ast.CosNode
+      expect(cos).toBeTruthy()
+      expect(cos.type).toBe(ast.NodeType.Cos)
+      assertMapAstNode(cos, 1, 0, 1, 5, 'cos(0)')
+      assertMapAstNode(cos.value, 1, 4, 1, 4, '0')
+    })
+
+    it('exp', () => {
+      const exp = execParseExpression('exp(0);') as ast.ExpNode
+      expect(exp).toBeTruthy()
+      expect(exp.type).toBe(ast.NodeType.Exp)
+      assertMapAstNode(exp, 1, 0, 1, 5, 'exp(0)')
+      assertMapAstNode(exp.value, 1, 4, 1, 4, '0')
+    })
+
+    it('floor', () => {
+      const floor = execParseExpression('floor(0);') as ast.FloorNode
+      expect(floor).toBeTruthy()
+      expect(floor.type).toBe(ast.NodeType.Floor)
+      assertMapAstNode(floor, 1, 0, 1, 7, 'floor(0)')
+      assertMapAstNode(floor.value, 1, 6, 1, 6, '0')
+    })
+
+    it('log', () => {
+      const log = execParseExpression('log(0);') as ast.LogNode
+      expect(log).toBeTruthy()
+      expect(log.type).toBe(ast.NodeType.Log)
+      assertMapAstNode(log, 1, 0, 1, 5, 'log(0)')
+      assertMapAstNode(log.value, 1, 4, 1, 4, '0')
+    })
+
+    it('pow', () => {
+      const pow = execParseExpression('pow(0,1);') as ast.PowNode
+      expect(pow).toBeTruthy()
+      expect(pow.type).toBe(ast.NodeType.Pow)
+      assertMapAstNode(pow, 1, 0, 1, 7, 'pow(0,1)')
+      assertMapAstNode(pow.x, 1, 4, 1, 4, '0')
+      assertMapAstNode(pow.y, 1, 6, 1, 6, '1')
+    })
+
+    it('rand', () => {
+      const rand = execParseExpression('rand(0);') as ast.RandNode
+      expect(rand).toBeTruthy()
+      expect(rand.type).toBe(ast.NodeType.Rand)
+      assertMapAstNode(rand, 1, 0, 1, 6, 'rand(0)')
+      assertMapAstNode(rand.value, 1, 5, 1, 5, '0')
+    })
+
+    it('empty argument rand', () => {
+      const rand = execParseExpression('rand();') as ast.RandNode
+      expect(rand).toBeTruthy()
+      expect(rand.type).toBe(ast.NodeType.Rand)
+      assertMapAstNode(rand, 1, 0, 1, 5, 'rand()')
+      expect(rand.value).toBeFalsy()
+    })
+
+    it('sin', () => {
+      const sin = execParseExpression('sin(0);') as ast.SinNode
+      expect(sin).toBeTruthy()
+      expect(sin.type).toBe(ast.NodeType.Sin)
+      assertMapAstNode(sin, 1, 0, 1, 5, 'sin(0)')
+      assertMapAstNode(sin.value, 1, 4, 1, 4, '0')
+    })
+
+    it('sqrt', () => {
+      const sqrt = execParseExpression('sqrt(0);') as ast.SqrtNode
+      expect(sqrt).toBeTruthy()
+      expect(sqrt.type).toBe(ast.NodeType.Sqrt)
+      assertMapAstNode(sqrt, 1, 0, 1, 6, 'sqrt(0)')
+      assertMapAstNode(sqrt.value, 1, 5, 1, 5, '0')
+    })
+
     it('var', () => {
       const variable = execParseExpression('$aBC_0;') as ast.VarNode
       expect(variable).toBeTruthy()
